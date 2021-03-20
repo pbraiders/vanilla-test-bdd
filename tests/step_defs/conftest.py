90600d9cea3db191e4c2e460281a460b9caa7557
ojullien <3778194+ojullien@users.noreply.github.com>
2021-03-20 17:16:33 +0100

    splinter
    Signed-off-by: ojullien <3778194+ojullien@users.noreply.github.com>

--- tests/step_defs/conftest.py
+++ tests/step_defs/conftest.py
@@ -3,24 +3,15 @@ This module contains shared fixtures, steps, and hooks.
 """
 
 import pytest
-#from selenium.webdriver import Chrome
 from splinter import Browser
-
 
 @pytest.fixture
 def theBrowser():
-  # Initialize ChromeDriver
   #driver = Browser('chrome', headless=True)
   pBrowser = Browser('chrome')
-  #driver.maximize_window()
-  # Wait implicitly for elements to be ready before attempting interactions
-  #pBrowser.implicitly_wait(10)
-  # Return the driver object at the end of setup
   yield pBrowser
-  # For cleanup, quit the driver
   pBrowser.quit()
 
 
-
-#def pytest_bdd_step_error(request, feature, scenario, step, step_func, step_func_args, exception):
-#    print(f'Step failed: {step}')
+def pytest_bdd_step_error(request, feature, scenario, step, step_func, step_func_args, exception):
+    print(f'Step failed: {step}')
