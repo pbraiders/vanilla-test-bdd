2372b992805a8f6fb6f81298f42e32c5dc854d80
ojullien <3778194+ojullien@users.noreply.github.com>
2021-03-20 19:11:08 +0100

    driver option
    Signed-off-by: ojullien <3778194+ojullien@users.noreply.github.com>

--- tests/step_defs/conftest.py
+++ tests/step_defs/conftest.py
@@ -5,13 +5,19 @@ This module contains shared fixtures, steps, and hooks.
 import pytest
 from splinter import Browser
 
+def pytest_addoption(parser):
+    parser.addoption('--driver', action='store', default='chrome', help='drivers choice: chrome or firefox')
+
+@pytest.fixture(scope="session", autouse=True)
+def driver(pytestconfig):
+    yield pytestconfig.getoption('driver')
+
 @pytest.fixture
-def theBrowser():
-  #driver = Browser('chrome', headless=True)
-  pBrowser = Browser('chrome')
+def theBrowser(driver):
+  #driver = Browser('driver', headless=True)
+  pBrowser = Browser(driver)
   yield pBrowser
   pBrowser.quit()
-
 
 def pytest_bdd_step_error(request, feature, scenario, step, step_func, step_func_args, exception):
     print(f'Step failed: {step}')
