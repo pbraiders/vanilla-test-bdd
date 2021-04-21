9079cdfef5f05c093f1e5e46986397fd3eb82e3c
ojullien <3778194+ojullien@users.noreply.github.com>
2021-04-21 16:33:54 +0200

    feat: add two new methods
    Signed-off-by: ojullien <3778194+ojullien@users.noreply.github.com>

--- tests/pbraiders/options/users/pageusers.py
+++ tests/pbraiders/options/users/pageusers.py
@@ -13,6 +13,7 @@ FIELD_PASSWD = 'pwd'
 FIELD_CONF_PASSWD = 'pwdc'
 BUTTON_SEND = 'new'
 FAILURE_MESSAGE = "Le nom d\'utilisateur ou les mots de passe que vous avez saisis sont incorrects."
+EXIST_MESSAGE = 'Cet utilisateur existe déjà.'
 SUCCESS_MESSAGE = "Enregistrement réussi."
 USER_LIST = ' • JAMAIS • Actif'
 
@@ -29,10 +30,12 @@ class PageUsers(object):
         self.user = user
         return self
 
-    def visit(self, stay_on_the_page: bool = False) -> PageUsers:
+    def on_page(self) -> bool:
+        """Test if we already are on the page"""
+        return self.browser.title == TITLE is True
+
+    def visit(self) -> PageUsers:
         """Goes to the page"""
-        if self.browser.title == TITLE and stay_on_the_page is True:
-            return self
         self.browser.visit(urljoin(str(self.config['home']), str(self.config['users'])))
         assert self.browser.title == TITLE
         return self
@@ -57,12 +60,18 @@ class PageUsers(object):
         self.browser.find_by_name(BUTTON_SEND).first.click()
         return self
 
+    def exists(self) -> bool:
+        """Test if the user already exists in the list"""
+        return self.browser.is_text_present(self.user.login + USER_LIST) is True
+
     def has_failed(self) -> bool:
         """Test if the new user creation has failed"""
-        return self.browser.is_text_present(FAILURE_MESSAGE) is True and self.browser.is_text_present(
-            self.user.login) is False
+        return self.browser.is_text_present(FAILURE_MESSAGE) is True and self.exists() is False
+
+    def has_failed_exist(self) -> bool:
+        """Test if the new user creation has failed because he's already exist"""
+        return self.browser.is_text_present(EXIST_MESSAGE) is True and self.exists() is True
 
     def has_succeeded(self) -> bool:
         """Test if the new user creation has successded"""
-        return self.browser.is_text_present(SUCCESS_MESSAGE) is True and self.browser.is_text_present(
-            self.user.login + USER_LIST) is True
+        return self.browser.is_text_present(SUCCESS_MESSAGE) is True and self.exists() is True
