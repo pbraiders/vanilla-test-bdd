d1871d0e59ddca464014267a2bef9daed29a79ec
ojullien <3778194+ojullien@users.noreply.github.com>
2021-05-06 21:42:54 +0200

    Feat: headcount features
    Signed-off-by: ojullien <3778194+ojullien@users.noreply.github.com>

---
+++ tests/features/options/parameters/headcount.feature
@@ -0,0 +1,14 @@
+@admin @parameters @headcount
+Feature: Update the maximum headcount per month.
+
+    Background:
+        Given I want to update the headcount per month
+
+    Scenario: Updating the headcounts
+        When I update the headcounts
+        Then I should see the success message
+        And The update should be permanent
+
+    Scenario: Not using a valid value
+        When I update the headcounts with a non valid value
+        Then I should see the error message
--- tests/pbraiders/options/parameters/pageparameters.py
+++ tests/pbraiders/options/parameters/pageparameters.py
@@ -10,11 +10,13 @@ from splinter import Browser
 TITLE = 'PBRaiders - Paramètres'
 HEADER = 'Paramètres'
 FIELD_PURGE = 'rey'
+FIELD_HEADCOUNT = 'pa{}'
 BUTTON_SEND = 'Envoyer'
+BUTTON_MODIFY = 'update'
 PURGE_FAILURE_MESSAGE = "La date saisie n'est pas valide."
 PURGE_SUCCESS_MESSAGE = "Suppression réussie."
-MAX_FAILURE_MESSAGE = "La date saisie n'est pas valide."
-MAX_SUCCESS_MESSAGE = "Suppression réussie."
+HEADCOUNT_FAILURE_MESSAGE = "Une des valeurs n'est pas valide."
+HEADCOUNT_SUCCESS_MESSAGE = "Enregistrement réussi."
 
 
 @dataclass
@@ -48,17 +50,37 @@ class PageParameters(object):
         """Test if the purge has succeded"""
         return self.browser.is_text_present(PURGE_SUCCESS_MESSAGE, wait_time=1)
 
-    def max(self, value: str = None) -> PageParameters:
+    def update_headcount(self) -> PageParameters:
         """Fills the  max update fields and clicks the button."""
-        if value is not None:
-            self.browser.fill(FIELD_PURGE, int(value))
-        self.browser.find_by_value(BUTTON_SEND).first.click()
+        self.browser.find_by_name(BUTTON_MODIFY).first.click()
         return self
 
-    def max_has_failed(self) -> bool:
-        """Test if the max update has failed"""
-        return self.browser.is_text_present(PURGE_FAILURE_MESSAGE, wait_time=1)
+    def headcounts(self) -> PageParameters:
+        """Fills the  max update fields."""
+        for i_month in range(1, 13, 1):
+            self.browser.fill(FIELD_HEADCOUNT.format(i_month), i_month)
+        return self
 
-    def max_has_succeeded(self) -> bool:
+    def check_headcounts(self) -> bool:
+        """Checks the headcount fields."""
+        b_return = True
+        for i_month in range(1, 13, 1):
+            i_value = int(self.browser.find_by_name(FIELD_HEADCOUNT.format(i_month)).first.value)
+            b_return = (b_return is True) and (i_value == i_month)
+        return b_return
+
+    def headcount(self, month: int, value) -> PageParameters:
+        """Fills the headcount update field."""
+        if 1 <= month <= 12:
+            self.browser.fill(FIELD_HEADCOUNT.format(month), value)
+        else:
+            raise TypeError("month is not valid!")
+        return self
+
+    def update_has_failed(self) -> bool:
+        """Test if the headcount update has failed"""
+        return self.browser.is_text_present(HEADCOUNT_FAILURE_MESSAGE, wait_time=1)
+
+    def update_has_succeeded(self) -> bool:
         """Test if the max update has succeded"""
-        return self.browser.is_text_present(PURGE_SUCCESS_MESSAGE, wait_time=1)
+        return self.browser.is_text_present(HEADCOUNT_SUCCESS_MESSAGE, wait_time=1)
