98e650f0dad1aed4b10c31dd5e2723e752e96bfb
ojullien <3778194+ojullien@users.noreply.github.com>
2021-05-19 18:35:49 +0200

    feat(user): add factory to create user using faker
    Signed-off-by: ojullien <3778194+ojullien@users.noreply.github.com>

---
+++ tests/pbraiders/user/userfakerfactory.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""Factory used to create simple user using Faker."""
+
+from __future__ import annotations
+from dataclasses import dataclass
+from faker import Faker
+from pbraiders.user import User
+from pbraiders.user import UserAbstractFactory
+
+
+@dataclass
+class UserFakerFactory(UserAbstractFactory):
+    """Factory used to create simple user using Faker."""
+
+    faker: Faker
+
+    def create(self, config: dict) -> User:
+        s_name = self.faker.first_name()
+        s_passwd = s_name + 'password'
+        return User(login=s_name, password=s_passwd, passwordc=s_passwd)
