ab54254965c8a35bfbd740cab8a0771ce1da5c3f
ojullien <3778194+ojullien@users.noreply.github.com>
2021-06-22 15:22:29 +0200

    refactor(users): split fat classes into responsability actions
    Signed-off-by: ojullien <3778194+ojullien@users.noreply.github.com>

--- tests/pbraiders/pages/__init__.py
+++ tests/pbraiders/pages/__init__.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from .contacts_utilities import new_contact
 from .page_abstract import PageAbstract
+from .contacts_utilities import new_contact
 from .signin_utilities import sign_in
 from .users_utilities import new_account
